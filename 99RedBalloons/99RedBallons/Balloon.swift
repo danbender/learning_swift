710f4d4cc0beb52627b11c7fc7b27cc4e0d54e42
Dan Bender <hello@danbender.net>
2015-06-09 09:00:28 +0200

    99redbaloons project

---
+++ 99RedBalloons/99RedBallons/Balloon.swift
@@ -0,0 +1,17 @@
+//
+//  Balloon.swift
+//  99RedBallons
+//
+//  Created by Dan Bender on 05/06/15.
+//  Copyright (c) 2015 Dan Bender. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon {
+    var image = UIImage(named: "")
+    var number = 0
+    
+      
+}
