a4e12a87fce55d646191495b13c1d0dc12f75da0
Dan Bender <hello@danbender.net>
2015-06-11 06:40:22 +0200

    created LionCub, which is a subclass of Lion

---
+++ LionsAndTigers/LionsAndTigers/LionCub.swift
@@ -0,0 +1,14 @@
+//
+//  LionCub.swift
+//  LionsAndTigers
+//
+//  Created by Dan Bender on 11/06/15.
+//  Copyright (c) 2015 Dan Bender. All rights reserved.
+//
+
+import Foundation
+
+
+class LionCub: Lion {
+    
+}
