2fd79e43bcc242f9b62bd041eb5cd7da8341562c
Dan Bender <hello@danbender.net>
2015-06-13 15:35:01 +0200

    setup secondContainer with 3x3 tiles

--- SlotMachine/SlotMachine/ViewController.swift
+++ SlotMachine/SlotMachine/ViewController.swift
@@ -32,6 +32,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.setupContainerViews()
         self.setupFirstContainer(self.firstContainer)
+        self.setupSecondContainer(self.secondContainer)
     }
 
     override func didReceiveMemoryWarning() {
