28f204bcceccc64c18ce70669df6f0c48d8b0d00
Dan Bender <hello@danbender.net>
2015-06-23 07:06:06 +0200

    slotMachine: added class func createSlots () to create 3 x 3 slots

--- SlotMachine/SlotMachine/Factory.swift
+++ SlotMachine/SlotMachine/Factory.swift
@@ -10,5 +10,23 @@ import Foundation
 import UIKit
 
 class Factory {
-    
+    class func createSlots () -> [[Slot]] {
+
+//        slots = [[slot1, slot2, slot3], [slot4, slot5, slot6], [slot7, slot8, slot9]]
+
+        let kNumberOfSlots = 3
+        let kNumberOfContainers = 3
+        var slots: [[Slot]] = []
+        for var containerNumber = 0; containerNumber < kNumberOfContainers; ++containerNumber {
+            var slotArray:[Slot] = []
+            for var slotNumber = 0; slotNumber < kNumberOfSlots; ++slotNumber {
+                var slot = Slot(value: 0, image: UIImage(named: ""), isRed: true)
+                slotArray.append(slot)
+            }
+            slots.append(slotArray)
+        }
+        return slots
+    }
 }
+
+
