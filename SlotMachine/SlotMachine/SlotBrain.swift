a3878b201e0d4680a3db9b5da408b40dcbcc4a10
Dan Bender <hello@danbender.net>
2015-07-04 10:44:12 +0400

    SlotMachineL: iterate over slotRows and unpack them

--- SlotMachine/SlotMachine/SlotBrain.swift
+++ SlotMachine/SlotMachine/SlotBrain.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+
+class SlotBrain {
+    
+    class func unpackSlotsIntoSlotRows (slots : [[Slot]]) -> [[Slot]] {
+        
+        var slotRow : [Slot] = []
+        var slotRow2 : [Slot] = []
+        var slotRow3 : [Slot] = []
+        
+        for slotArray in slots {
+            for var index = 0; index < slotArray.count; ++index {
+                let slot = slotArray[index]
+                if index == 0 {
+                    slotRow.append(slot)
+                }
+                else if index == 1 {
+                    slotRow2.append(slot)
+                    
+                }
+                else if  index == 2 {
+                    slotRow3.append(slot)
+                }
+                else {
+                    println("Error, invalid slot")
+                }
+            }
+        }
+        
+        var slotsInRows : [[Slot]] = [slotRow, slotRow2, slotRow3]
+        
+        return slotsInRows
+    }
+    
+}
