45858598108dbe2a425716c56dfe66ba305ee4c4
Dan Bender <hello@danbender.net>
2015-07-21 22:39:24 +0200

    initial project setup, created struct

---
+++ LemonadeStand/LemonadeStand/Supplies.swift
@@ -0,0 +1,22 @@
+//
+//  Supplies.swift
+//  LemonadeStand
+//
+//  Created by Dan Bender on 21/07/15.
+//  Copyright (c) 2015 Dan Bender. All rights reserved.
+//
+
+import Foundation
+
+struct Supplies {
+    var money = 0
+    var lemons = 0
+    var iceCubes = 0
+    
+//    custom Initializers
+    init (aMoney : Int, aLemons : Int, aIceCubes: Int) {
+        money = aMoney
+        
+    }
+    
+}
