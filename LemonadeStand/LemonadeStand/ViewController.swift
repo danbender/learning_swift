bfda0c633d7e1c5cc4e9f5248dada5eb2ab547a7
Dan Bender <hello@danbender.net>
2015-07-28 18:59:38 +0200

    LemonadeStand: add alert with text

--- LemonadeStand/LemonadeStand/ViewController.swift
+++ LemonadeStand/LemonadeStand/ViewController.swift
@@ -88,6 +88,15 @@ class ViewController: UIViewController {
         iceCubeMixCount.text = "\(iceCubesToMix)"
     }
     
+    func showAlertWithText (header : String = "Warning", message : Sring) {
+    
+        var alert = UIAlertController(title: header, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
     
     
     
