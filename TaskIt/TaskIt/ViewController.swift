ff7a0d3c5334afdb5aa058bcce768c6c7eec9e48
Dan Bender <hello@danbender.net>
2015-08-03 22:47:25 +0200

    taskIt: UITableViewDataSource

--- TaskIt/TaskIt/ViewController.swift
+++ TaskIt/TaskIt/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+//    UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 
 }
 
