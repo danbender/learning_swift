13b068f1cb8b5867d5924aa5e69e1816d5f600f1
Dan Bender <hello@danbender.net>
2015-08-08 23:22:26 +0200

    taskIt: Refactor

--- TaskIt/TaskIt/ViewController.swift
+++ TaskIt/TaskIt/ViewController.swift
@@ -9,10 +9,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let task1:Dictionary<String, String> = ["task": "Study French", "subtask": "Verbs", "date": "01/14/2014"]
-        let task2:Dictionary<String, String> = ["task": "Eat Dinner", "subtask": "Burgers", "date": "01/14/2014"]
-        let task3:Dictionary<String, String> = ["task": "Gym", "subtask": "Leg day", "date": "01/14/2014"]
-        taskArray = [task1, task2, task3]
+        let task1 = TaskModel(task : "Study French", subTask: "Verbs", date: "01/01/2014")
+        let task2 = TaskModel(task: "Eat Dinner", subTask: "Burgers", date: "01/01/2014")
+        
+// alterantively create instances directly in array:
+        taskArray = [task1, task2, TaskModel(task: "Gym", subTask: "Leg day", date: "01/01/2014")]
         
         self.tableView.reloadData()
     }
@@ -30,15 +31,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        println(indexPath.row)
-        
-        let taskDict:Dictionary = taskArray[indexPath.row]
+        let thisTask = taskArray[indexPath.row]
         
         var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as! TaskCell
         
-        cell.taskLabel.text = taskDict["task"]
-        cell.descriptionLabel.text = taskDict["subtask"]
-        cell.dateLabel.text = taskDict["date"]
+        cell.taskLabel.text = thisTask.task
+        cell.descriptionLabel.text = thisTask.subTask
+        cell.dateLabel.text = thisTask.date
         
         return cell
     }
