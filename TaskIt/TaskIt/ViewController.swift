b0499d873ebfd841aa8c5953b1b5e06c86842601
Dan Bender <hello@danbender.net>
2015-08-08 23:09:25 +0200

    TaskIt: using dictionary + array to encapsulate info about task, iterate through array, show

--- TaskIt/TaskIt/ViewController.swift
+++ TaskIt/TaskIt/ViewController.swift
@@ -1,20 +1,20 @@
-//
-//  ViewController.swift
-//  TaskIt
-//
-//  Created by Dan Bender on 08/08/15.
-//  Copyright (c) 2015 Dan Bender. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var taskArray:[Dictionary<String, String>] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let task1:Dictionary<String, String> = ["task": "Study French", "subtask": "Verbs", "date": "01/14/2014"]
+        let task2:Dictionary<String, String> = ["task": "Eat Dinner", "subtask": "Burgers", "date": "01/14/2014"]
+        let task3:Dictionary<String, String> = ["task": "Gym", "subtask": "Leg day", "date": "01/14/2014"]
+        taskArray = [task1, task2, task3]
+        
+        self.tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,11 +24,23 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
 //    UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+       
+        return taskArray.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        println(indexPath.row)
+        
+        let taskDict:Dictionary = taskArray[indexPath.row]
+        
+        var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as! TaskCell
+        
+        cell.taskLabel.text = taskDict["task"]
+        cell.descriptionLabel.text = taskDict["subtask"]
+        cell.dateLabel.text = taskDict["date"]
+        
+        return cell
     }
     
 //    UITableViewDelegate
