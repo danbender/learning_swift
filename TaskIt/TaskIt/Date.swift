4c226c61a3602e553435d7db57b625c2aac0a65d
Dan Bender <hello@danbender.net>
2015-08-14 08:18:05 +0200

    TaskIt: writing NSDate

---
+++ TaskIt/TaskIt/Date.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+class Date {
+    class func from (#year:Int, month:Ind, day:Int) -> NSDate {
+        
+        return NSDate()
+    }
+}
--- TaskIt/TaskIt/TaskModel.swift
+++ TaskIt/TaskIt/TaskModel.swift
@@ -1,11 +1,3 @@
-//
-//  TaskModel.swift
-//  TaskIt
-//
-//  Created by Dan Bender on 08/08/15.
-//  Copyright (c) 2015 Dan Bender. All rights reserved.
-//
-
 import Foundation
 
 struct TaskModel {
