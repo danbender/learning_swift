417f2cab86467cb449385ea595a710547ce2f7ba
Dan Bender <hello@danbender.net>
2015-08-15 17:45:07 +0200

    connect addTask outlets from storyboard to controller; add a reference to the ViewController object in the AddTaskViewController, so we have access to it.

--- TaskIt/TaskIt/AddTaskViewController.swift
+++ TaskIt/TaskIt/AddTaskViewController.swift
@@ -1,6 +1,12 @@
 import UIKit
 
 class AddTaskViewController: UIViewController {
+    
+    var mainVC: ViewController!
+    
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var subTaskTextField: UITextField!
+    @IBOutlet weak var dueDatePicker: UIDatePicker!
 
     @IBAction func cancelButtonTapped(sender: UIButton) {
         self.dismissViewControllerAnimated(true, completion: nil)
