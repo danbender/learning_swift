d67390070ee59c9e6c5abfd775bd8d4402eafa8f
Dan Bender <hello@danbender.net>
2015-09-28 19:48:17 +0200

    set up transitions from LoginViewController to CreateAccountViewController and MainViewController

--- LoginProject/LoginProject/LoginViewController.swift
+++ LoginProject/LoginProject/LoginViewController.swift
@@ -17,9 +17,10 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginButtonPressed(sender: UIButton) {
+        self.performSegueWithIdentifier("loginToMainSegue", sender: self)
     }
    
     @IBAction func createAccountButtonPressed(sender: UIButton) {
+        self.performSegueWithIdentifier("loginToCreateAccountSegue", sender: self)
     }
-
 }
