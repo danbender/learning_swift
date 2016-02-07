4b00681c82a6d41cfefa2699a86df83bbd3e6e89
Dan Bender <hello@danbender.net>
2016-02-07 08:42:31 +0100

    refactor

--- physics-anmiation/physics-anmiation/ViewController.swift
+++ physics-anmiation/physics-anmiation/ViewController.swift
@@ -28,7 +28,6 @@ class ViewController: UIViewController {
         welcomeLabel.font = UIFont.systemFontOfSize(36)
         welcomeLabel.sizeToFit()
 
-        
         welcomeLabel.center = CGPoint(x:100, y:240)
         view.addSubview(welcomeLabel)
         
@@ -38,7 +37,6 @@ class ViewController: UIViewController {
         nameLabel.font = UIFont.systemFontOfSize(48)
         nameLabel.sizeToFit()
 
-        
         nameLabel.center = CGPoint(x: 200, y: 290)
         view.addSubview(nameLabel)
     }
@@ -47,41 +45,33 @@ class ViewController: UIViewController {
         welcomeLabel.center = CGPoint(x: 100, y: 40)
         
         UIView.animateWithDuration(0.5, animations: { () -> Void in
-           
             self.welcomeLabel.center = CGPoint(x: 100, y: 240)
             self.welcomeLabel.alpha = 1
-            
         }, completion: nil)
+        
         
         nameLabel.center = CGPoint(x: 200, y: 90)
         
         UIView.animateWithDuration(0.5, animations: { () -> Void in
-          
             self.nameLabel.center = CGPoint(x: 200, y: 290)
             self.nameLabel.alpha = 1
-        
         }, completion: nil)
     }
     
     func animateLabelsWithPhysics() {
         welcomeLabel.center = CGPoint(x: 100, y: 40)
-        welcomeLabel.alpha = 0
         
         UIView.animateWithDuration(0.2, delay: 0.3, usingSpringWithDamping: 0.9, initialSpringVelocity: 50.8, options: [], animations: { () -> Void in
-            
             self.welcomeLabel.center = CGPoint(x: 100, y: 240)
             self.welcomeLabel.alpha = 1
-            
         }, completion: nil)
+     
         
         nameLabel.center = CGPoint(x: 200, y: 90)
-        nameLabel.alpha = 0
         
         UIView.animateWithDuration(1.0, delay: 0.5, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: [], animations: { () -> Void in
-            
             self.nameLabel.center = CGPoint(x: 200, y: 290)
             self.nameLabel.alpha = 1
-            
         }, completion: nil)
         
     }
