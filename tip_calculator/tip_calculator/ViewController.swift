29e49d3bc4b510f97ad208dd0e7783b376b9fa9f
Dan Bender <hello@danbender.net>
2016-02-17 15:25:05 +0100

    dev

--- tip_calculator/tip_calculator/ViewController.swift
+++ tip_calculator/tip_calculator/ViewController.swift
@@ -8,16 +8,47 @@
 
 import UIKit
 
+class tipCalculator {
+    var currentTip: String = ""
+    
+}
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userValueInputTextField: UITextField!
+    
+    @IBOutlet weak var tipPercentage: UILabel!
+    @IBOutlet weak var tipValue: UILabel!
+    
+    @IBOutlet weak var totalAmount: UILabel!
+    
+    @IBOutlet weak var tipPercentageSlider: UISlider!
+    
+    var chosenTipValue : Int = 0
+    var currentSubtotal : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        getCurrentSubtotal()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func tipPercentageSliderAction(sender: UISlider) {
+        chosenTipValue = Int(sender.value)
+        
+        tipPercentage.text = "Tip (\(tipValue)%):"
+        tipPercentage.sizeToFit()
+    }
+    
+    func getCurrentSubtotal () {
+        currentSubtotal = userValueInputTextField.text!
+        print(currentSubtotal)
+        resignFirstResponder()
     }
 
 
