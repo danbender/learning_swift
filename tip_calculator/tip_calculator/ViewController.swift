1ca86ddc89378e827faf8858b43b0dc9eaae2cbb
Dan Bender <hello@danbender.net>
2016-02-18 09:36:18 +0100

    basic tip calc app works

--- tip_calculator/tip_calculator/ViewController.swift
+++ tip_calculator/tip_calculator/ViewController.swift
@@ -8,10 +8,6 @@
 
 import UIKit
 
-class tipCalculator {
-    var currentTip: String = ""
-    
-}
 class ViewController: UIViewController {
 
     @IBOutlet weak var userValueInputTextField: UITextField!
@@ -23,9 +19,21 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tipPercentageSlider: UISlider!
     
+    var chosentipValue : Int = 0
+    var userSubtotalInput : Int = 0
+    var tipAmount : Double = 0.0
+    let currencyFormatter = NSNumberFormatter()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        let textField = userValueInputTextField
+//        textField.addTarget(self, action: "textFieldDidChange:", forControlEvents: UIControlEvents.EditingChanged)
+//        currencyFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+//        currencyFormatter.currencyCode = NSLocale.currentLocale().displayNameForKey(NSLocaleCurrencySymbol, value: NSLocaleCurrencyCode)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,11 +42,50 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tipPercentageSliderAction(sender: UISlider) {
-        var chosentipValue = Int(sender.value)
+        chosentipValue = Int(sender.value)
         tipPercentage.text = "Tip (\(chosentipValue)%):"
         
+        calculateTipAmount()
+        calculateTotalAmount()
     }
+    
+    @IBAction func TipMeButtonTapped(sender: AnyObject) {
+        userSubtotalInput = Int(userValueInputTextField.text!)!
+    }
+    
+
+    
+    
+//    MARK - helper functions
+    
+    
+    func calculateTipAmount () {
+        tipAmount = (Double(userSubtotalInput)/100) * Double(chosentipValue)
+        tipValue.text = "\(tipAmount)"
+    }
+    
+    func calculateTotalAmount () {
+        totalAmount.text = "$ \(Double(tipAmount) + Double(userSubtotalInput))"
+    }
+    
+    
+    
+//    func textFieldDidChange(textField: UITextField) {
+//        var text = textField.text!.stringByReplacingOccurrencesOfString(currencyFormatter.currencySymbol, withString: "").stringByReplacingOccurrencesOfString(currencyFormatter.groupingSeparator, withString: "").stringByReplacingOccurrencesOfString(currencyFormatter.decimalSeparator, withString: "")
+//        textField.text = currencyFormatter.stringFromNumber((text as NSString).doubleValue / 100.0)
+//    }
+//    
+//    
+//    func currencyStringAsDouble(currencyString: String) -> Double {
+//        let cleanedString = currencyString.stringByReplacingOccurrencesOfString(currencyFormatter.currencySymbol, withString: "").stringByReplacingOccurrencesOfString(currencyFormatter.groupingSeparator, withString: "").stringByReplacingOccurrencesOfString(currencyFormatter.decimalSeparator, withString: "")
+//        let currencyDouble = (text as NSString).doubleValue / 100.0
+//        return currencyDouble;
+//    }
 
 
 }
 
+
+// get
+//  subtotal + tipamount = total
+//         subtotal * tipercentage = tip amount
